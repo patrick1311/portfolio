73cc1202d1c34b30859da9bbf15dfa6414fc8804
Patrick Quach <patrickquach96@gmail.com>
2020-02-20 00:29:50 -0800

    add profile pic

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route} from 'react-router-dom';
+import { BrowserRouter as Router, HashRouter, Route} from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './components/Home';
@@ -9,14 +9,14 @@ import Contact from './components/Contact';
 
 function App() {
   return (
-    <Router>
+    <HashRouter>
       <Header/>
       <Route exact path='/' component={Home} />
       <Route path='/experience' component={Experience} />
       <Route path='/projects' component={Projects} />
       <Route path='/contact' component={Contact} />
       <Footer/>
-    </Router>
+    </HashRouter>
   );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Button from '@material-ui/core/Button';
+import profilepic from '../img/profilepic.png';
 
 const navBarStyle = makeStyles({
     navBar: {
@@ -21,6 +22,14 @@ const navBarStyle = makeStyles({
         width: '100%',
         height: '100%',
         borderRadius: '0%',
+    },
+    imgBlock: {
+        textAlign: 'center',
+        backgroundColor: '#000000'
+    },
+    img: {
+        verticalAlign: 'bottom',
+        width: '200px',
     }
 });
 
@@ -28,24 +37,28 @@ const Header = () => {
     const classes = navBarStyle();
     return (
         <div>
+            <div className={classes.imgBlock}>
+                <img className={classes.img} src={profilepic} />
+            </div>
+
             <AppBar className={classes.navBar} position="static">
-                <Link className={classes.link} to="/home">
-                    <Button className={classes.button} variant="contained" color="primary" href="#contained-buttons">
+                <Link className={classes.link} to="/">
+                    <Button className={classes.button} variant="contained" color="primary">
                         Home
                     </Button>
                 </Link>
                 <Link className={classes.link} to="/experience">
-                    <Button className={classes.button} variant="contained" color="primary" href="#contained-buttons">
+                    <Button className={classes.button} variant="contained" color="primary">
                         Experience
                     </Button>
                 </Link>
                 <Link className={classes.link} to="/projects">
-                    <Button className={classes.button} variant="contained" color="primary" href="#contained-buttons">
+                    <Button className={classes.button} variant="contained" color="primary">
                         Projects
                     </Button>
                 </Link>
                 <Link className={classes.link} to="/contact">
-                    <Button className={classes.button} variant="contained" color="primary" href="#contained-buttons">
+                    <Button className={classes.button} variant="contained" color="primary">
                         Contact
                     </Button>
                 </Link>
